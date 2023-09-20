49dc18e0c13e2d06d182829eff4624aa35ac1150
dimicorn <zagorulia.ds@phystech.edu>
2023-09-20 23:16:53 +0300

    bash script edit

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ $1 -I/opt/homebrew/Cellar/sfml/2.6.0/include -o out -L/opt/homebrew/Cellar/sfml/2.6.0/lib -lsfml-graphics -lsfml-window -lsfml-system -std=c++20 -lgsl
+g++ $1 -I/opt/homebrew/Cellar/sfml/2.6.0/include -I/opt/homebrew/Cellar/gsl/2.7.1/include -o out -L/opt/homebrew/Cellar/sfml/2.6.0/lib -lsfml-graphics -lsfml-window -lsfml-system -std=c++20 -lgsl
